c4d39bf0355079c9f2a99990378acc739ea61696
Kasualix <670067575@qq.com>
2023-10-09 13:05:39 +0800

    0.0.2

--- src/main/java/com/teampotato/arrowcollector/mixin/MixinBowItem.java
+++ src/main/java/com/teampotato/arrowcollector/mixin/MixinBowItem.java
@@ -2,21 +2,23 @@ package com.teampotato.arrowcollector.mixin;
 
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.item.ArrowItem;
 import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(BowItem.class)
 public abstract class MixinBowItem {
-    @Redirect(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addFreshEntity(Lnet/minecraft/entity/Entity;)Z"))
-    private boolean onReleaseUsing(World instance, Entity entity) {
-        if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, ((BowItem)(Object)this).getDefaultInstance()) == 0) {
-            ((AbstractArrowEntity)entity).pickup = AbstractArrowEntity.PickupStatus.ALLOWED;
-        }
-        return instance.addFreshEntity(entity);
+    @Inject(method = "releaseUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addFreshEntity(Lnet/minecraft/entity/Entity;)Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    private void onReleaseUsing(ItemStack pStack, World pLevel, LivingEntity pEntityLiving, int pTimeLeft, CallbackInfo ci, PlayerEntity playerentity, boolean flag, ItemStack itemstack, int i, float f, boolean flag1, ArrowItem arrowitem, AbstractArrowEntity entity, int j, int k) {
+        if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, pStack) == 0) entity.pickup = AbstractArrowEntity.PickupStatus.ALLOWED;
     }
 }
