dc9db4534a7628c7c6b06bdf7c9eef49a7a63da2
Kasualix <670067575@qq.com>
2023-10-09 13:22:31 +0800

    0.0.2

--- src/main/java/com/teampotato/arrowcollector/mixin/MixinBowItem.java
+++ src/main/java/com/teampotato/arrowcollector/mixin/MixinBowItem.java
@@ -2,21 +2,23 @@ package com.teampotato.arrowcollector.mixin;
 
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.ArrowItem;
 import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(BowItem.class)
 public abstract class MixinBowItem {
-    @Redirect(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z"))
-    private boolean onStopUsing(World instance, Entity entity) {
-        if (EnchantmentHelper.getLevel(Enchantments.INFINITY, ((BowItem)(Object)this).getDefaultStack()) == 0) {
-            ((PersistentProjectileEntity)entity).pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
-        }
-        return instance.spawnEntity(entity);
+    @Inject(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    private void onStopUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci, PlayerEntity playerEntity, boolean bl, ItemStack itemStack, int i, float f, boolean bl2, ArrowItem arrowItem, PersistentProjectileEntity persistentProjectileEntity) {
+        if (EnchantmentHelper.getLevel(Enchantments.INFINITY, stack) == 0) persistentProjectileEntity.pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
     }
 }
